811bb74853d76feef36f63f6a3be57b528f59400
Aaron Baker <aaron.baker@neo.com>
2014-01-27 10:28:47 -0500

    fix some deploy setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,30 +35,31 @@ namespace :deploy do
   desc 'Start thin servers'
   task :start do
     on roles(:app) do
-      execute "cd #{fetch(:release_path)} && RUBY_GC_MALLOC_LIMIT=90000000 bundle exec thin -C config/thin.yml start"
+      execute "cd #{fetch(:release_path)} && PATH=/home/deployer/.rbenv/shims:/home/deployer/.rbenv/bin:$PATH RUBY_GC_MALLOC_LIMIT=90000000 bundle exec thin -C config/thin.yml start"
     end
   end
 
   desc 'Stop thin servers'
   task :stop do
     on roles(:app) do
-      execute "cd #{fetch(:release_path)} && bundle exec thin -C config/thin.yml stop"
+      execute "cd #{fetch(:release_path)} && PATH=/home/deployer/.rbenv/shims:/home/deployer/.rbenv/bin:$PATH bundle exec thin -C config/thin.yml stop"
     end
   end
 
   desc 'Restart thin servers'
   task :restart do
     on roles(:app) do
-      execute "cd #{fetch(:release_path)} && RUBY_GC_MALLOC_LIMIT=90000000 bundle exec thin -C config/thin.yml restart"
+      execute "cd #{fetch(:release_path)} && PATH=/home/deployer/.rbenv/shims:/home/deployer/.rbenv/bin:$PATH RUBY_GC_MALLOC_LIMIT=90000000 bundle exec thin -C config/thin.yml restart"
     end
   end
 end
 
-task :setup_shared_files => ["config/database.yml", "config/redis.yml", "config/discourse.conf"]
+task :setup_shared_files => ["config/database.yml", "config/redis.yml", "config/discourse.conf", "config/mandrill.yml"]
 
 remote_file "config/database.yml" => "config/database.yml.deploy", :roles => :app
-remote_file "config/redis.yml" => "config/redis.yml.deploy", :roles => :app
 remote_file "config/discourse.conf" => "config/discourse.conf.deploy", :roles => :app
+remote_file "config/mandrill.yml" => "config/mandrill.yml.deploy", :roles => :app
+remote_file "config/redis.yml" => "config/redis.yml.deploy", :roles => :app
 
 after "deploy:check:make_linked_dirs", "setup_shared_files"
 
