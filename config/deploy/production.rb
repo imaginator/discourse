dbb052cd7d084dde0bcf4d2e753363b87caa870b
Aaron Baker <aaron.baker@neo.com>
2014-01-31 14:04:31 -0500

    add db role

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-server '212.71.255.114', user: 'deployer', roles: %w{web app}
+server '212.71.255.114', user: 'deployer', roles: %w{web app db}
 set :rails_env, 'production'
 set :branch, "production"
 set :deploy_to, "/home/deployer/1net-forum"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :stage, :production
-server '176.58.98.19', user: 'deployer', roles: %w{web app}
+server '176.58.98.19', user: 'deployer', roles: %w{web app db}
 set :rails_env, 'production'
 set :branch, "master"
 set :deploy_to, "/home/deployer/1net-forum-staging"
